7226c62ddeee4094e75442fce9737708eb4d8221
soumyabiswas250498 <soumyabiswas250498@gmail.com>
2024-07-20 20:21:28 +0530

    multiple origin setup done

--- src/app.js
+++ src/app.js
@@ -9,11 +9,21 @@ import hwRouter from './routes/hw.routes.js';
 
 const app = express();
 
+const allowedOrigins = process.env.CORS_ORIGINS.split(',');
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
-}))
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cookieParser());
